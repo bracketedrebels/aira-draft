2fa052a642f566692aa62ef0544a48f6e5fd0a07
wolfson.rebel <wolfson.rebel@gmail.com>
2016-11-11 14:52:26 +0700

    editor rename

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'impex',
+  selector: 'airadraft',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
