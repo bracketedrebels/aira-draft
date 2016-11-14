93be7177a13b425bf7d3d581a3463b0f6c28fcb0
wolfson <wolfson@bracketedrebels.com>
2016-11-15 00:37:12 +0700

    completer integrated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 
+import { CompleterDomain } from './components/completer';
+
 @Component({
   selector: 'airadraft',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  providers: [ CompleterDomain ]
 })
 export class AppComponent {
-  
+  public processInput(aEvent: Event): void {
+    console.log(aEvent);
+  }
+
+  public handleKeyPress(aEvent: Event): void {
+    console.log(aEvent);
+  }
+
+  constructor(private domain: CompleterDomain) {}
 }
