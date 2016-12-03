8ef04fd4adffbd904d0538c22875e52ee35ac2a4
wolfson <wolfson@bracketedrebels.com>
2016-12-04 00:10:22 +0700

    emph pipe integrated with moule

--- src/app/modules/completer/completer.module.ts
+++ src/app/modules/completer/completer.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { CompleterComponent } from './completer.component';
+import { EmphPipe } from './emph.pipe';
 
 @NgModule({
   imports: [ CommonModule ],
-  declarations: [ CompleterComponent ],
+  declarations: [ CompleterComponent, EmphPipe ],
   exports: [ CompleterComponent ]
 })
 export class CompleterModule { }
